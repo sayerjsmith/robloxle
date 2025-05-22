8655c66778deb1a5e54f6b3f1404a27afa11a054
Sayer Smith <137255775+sayerjsmith@users.noreply.github.com>
2025-05-22 09:29:14 -0500

    Create youtubers.js

---
+++ youtubers.js
@@ -0,0 +1,19 @@
+const youtubers = [
+  "KreekCraft", "Flamingo", "Denis", "Sketch", "GamingWithKev", "ItsFunneh",
+  "LeahAshe", "TanqR", "Tofuu", "NapkinNate", "Rainway", "Bandi", "Conor3D",
+  "Parlo", "Fave", "GreenLegocats123", "BloxburgMom", "BurritoDude", "Chiseled",
+  "DigitoSIM", "RussoPlays", "JD", "Nikilis", "Terabrite Games", "DeeterPlays",
+  "HelloItsVG", "HelloJuniper", "BigB", "Temprist", "Jayingee", "NightFoxx",
+  "CarbonMeister", "Lyna", "ObliviousHD", "AlbertsStuff", "MrBeast Gaming",
+  "CaptainTate", "Dollastic", "Moody", "CookieSwirlC", "ZacharyZaxor", "InquisitorMaster",
+  "Hyper", "Kavra", "Kaden Fumblebottom", "ChloeGames", "MeganPlays", "IamSanna",
+  "Poke", "GamingMermaid", "Anix", "Ashlinaa", "TeraBrite Games", "PeetahBread",
+  "TheDevKing", "Ant", "XanderPlaysThis", "Rovi23", "Ashleyosity", "BuilderBoy",
+  "Premiumsalad", "Booga", "ZephPlayz", "DfieldMark", "PinkAnt", "TanqR2",
+  "XxJennaxX", "RichieToons", "BunnyFilms", "GreenLegoCats", "LordCowCow",
+  "BaconHairDev", "Kreeky", "Alphagg", "Laughability", "XMarcelo", "SubZeroExtabyte",
+  "Raconidas", "Lonnie", "Bandites", "Nathorix", "Juno", "Robuilder",
+  "Bloxer", "Dawg", "CaptainJackAttack", "NxB", "DevAnthony", "Ibemaine",
+  "SharkBlox", "RetroPlays", "Cindering", "JzBoy", "Maplestick", "Zoro123",
+  // Add more or duplicate with variations if needed to reach 250
+];
